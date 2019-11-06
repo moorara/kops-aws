decebc816152cfbb9c18d944145a7f94d81a9fe1
Milad Irannejad <moorara@gmail.com>
2019-11-06 01:17:06 -0500

    Update infra-terraform project outputs

--- infra-terraform/outputs.tf
+++ infra-terraform/outputs.tf
@@ -1,8 +1,23 @@
 # https://www.terraform.io/docs/configuration/outputs.html
 
+output "environment" {
+  value       = var.environment
+  description = "The Environment name for deployment."
+}
+
+output "region" {
+  value       = var.region
+  description = "The AWS Region for deployment."
+}
+
 output "availability_zones" {
   value       = join(",", slice(data.aws_availability_zones.available.names, 0, local.az_len))
-  description = "A comma-separated list of availability zones."
+  description = "A comma-separated string of availability zones."
+}
+
+output "availability_zones_list" {
+  value       = slice(data.aws_availability_zones.available.names, 0, local.az_len)
+  description = "The list of availability zones."
 }
 
 output "vpc_id" {
@@ -17,12 +32,30 @@ output "vpc_cidr" {
 
 output "private_subnet_ids" {
   value       = join(",", aws_subnet.private.*.id)
-  description = "A comma-separated list of private subnet IDs"
+  description = "A comma-separated string of private subnet IDs."
+}
+
+output "private_subnets_list" {
+  description = "A list of private subnets."
+  value = [for subnet in aws_subnet.private: {
+    id                = subnet.id
+    availability_zone = subnet.availability_zone
+    cidr              = subnet.cidr_block
+  }]
 }
 
 output "public_subnet_ids" {
   value       = join(",", aws_subnet.public.*.id)
-  description = "A comma-separated list of public subnet IDs"
+  description = "A comma-separated string of public subnet IDs."
+}
+
+output "public_subnets_list" {
+  description = "A list of public subnets."
+  value = [for subnet in aws_subnet.public: {
+    id                = subnet.id
+    availability_zone = subnet.availability_zone
+    cidr              = subnet.cidr_block
+  }]
 }
 
 output "kops_subdomain" {
@@ -42,5 +75,5 @@ output "kops_encrypted_secret" {
 
 output "resource_tags" {
   value       = join(",", [ for k, v in merge(local.common_tags, local.region_tag) : format("%s=%s", k, v) ])
-  description = "A comma-separated list of AWS tags."
+  description = "A comma-separated string of AWS tags."
 }
